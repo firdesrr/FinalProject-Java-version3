d250a23f3dcb285027afe17c92b1061ff4a6ffec
firdesrr <firdesrr@abv.bg>
2022-07-11 18:44:14 +0300

    printAllLeaveRequestsWithIdAndStatus

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
+
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.File;
-import java.io.IOException;
+
 
 //Курсов проект "Система за отпуски"
 public class Main {
     public static void printMenu() {
         String menu = "--------------------------------------------------------------------\n" +
-                "1. Заяви отпуска\n" +
-                "2. Виж всички отпуски\n" +
-                "3. Виж отпуска за служител\n" +
-                "4. Промени статус на отпуска\n" +
-                "5. Изход\n" +
+                "1. Request leave\n" +
+                "2. See all leaves\n" +
+                "3. See an employee leave\n" +
+                "4. Change leave status\n" +
+                "5. Exit\n" +
                 "-------------------------------------------------------------------\n";
         System.out.println(menu);
     }
 
-    //При избиране на 1, потребителят въвежда последователно
+ //При избиране на 1, потребителят въвежда последователно
 //     Име, имейл, ЕГН, две дати - за начало и край на отпуската, тип на отпуската - платена или неплатена.
 //     При грешен формат на данните да излизат подходящи съобщения.
     public static String[] inputLeaveData() {
@@ -25,8 +26,12 @@ public class Main {
         employeeData[0] = scan.nextLine();
         System.out.print("email: ");
         employeeData[1] = scan.nextLine();
-        System.out.print("personalIDNum: ");
+        System.out.print("personalIDNum: "); //провери дали дължината на този стринг е от 10 знака (всяко ЕГН съдържа 10 знака)
         employeeData[2] = scan.nextLine();
+        while (employeeData[2].length() != 10) {
+            System.out.println("Invalid PIN");
+            employeeData[2] = scan.nextLine();
+        }
         System.out.print("startDate (dd-mm-yyyy): ");
         employeeData[3] = scan.nextLine();
         System.out.print("endDate (dd-mm-yyyy): ");
@@ -38,29 +43,47 @@ public class Main {
     }
 
     //При избиране на 2, на екрана се показват във формата на таблица, всички         направени до сега заявки.
-
-    //    KAK???? Всички заявки се помнят при изключване на програмата.
-    public static String[][] fillMatrixLeaveData(int n) {
+    //   Всички заявки се помнят при изключване на програмата.
+    public static String[][] fillMatrixLeaveData(int numLeaveRequests) {
         //n брой заявени отпуски
-        //matrixLeaveData да е ArrayList от масив от стрингове или n да е 1000
-        //параметър за поредната попълвана отпуска, който ще се пази в main
-
-        String[][] matrixLeaveData = new String[n][6];
+        String[][] matrixLeaveData = new String[numLeaveRequests][8];
         String[] employeeData;
-
-        for (int i = 0; i <n; i++) {
-            employeeData= inputLeaveData();
+        int idLeaveRequest = 0;
+        for (int i = 0; i < numLeaveRequests; i++) {
+            employeeData = inputLeaveData();
             for (int j = 0; j < 6; j++) {
                 matrixLeaveData[i][j] = employeeData[j];
             }
+            idLeaveRequest++;
+            matrixLeaveData[i][6] = Integer.toString(idLeaveRequest);
+            matrixLeaveData[i][7] = "pending";
         }
         return matrixLeaveData;
     }
 
-    public static void printAllLeaveRequests(String[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
-                System.out.print(arr[i][j] + "   ");
+    public static void printAllLeaveRequests(String[][] matrixLeaveData) {
+       /* String [] matrixHeader={"Name", "email","PIN","start date", "end date", "type: paid/unpaid"};
+        for (String m : matrixHeader) {
+            System.out.print(m + "; ");
+        }
+        System.out.println( );*/
+        for (int i = 0; i < matrixLeaveData.length; i++) {
+            for (int j = 0; j < matrixLeaveData[0].length - 2; j++) {
+                System.out.print(matrixLeaveData[i][j] + "   ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printAllLeaveRequestsWithIdAndStatus(String[][] matrixLeaveData) {
+       /* String [] matrixHeader={"Name", "email","PIN","start date", "end date", "type: paid/unpaid", "idLeaveRequest", "Status"};
+        for (String m : matrixHeader) {
+            System.out.print(m + "; ");
+        }
+        System.out.println( );*/
+        for (int i = 0; i < matrixLeaveData.length; i++) {
+            for (int j = 0; j < matrixLeaveData[0].length; j++) {
+                System.out.print(matrixLeaveData[i][j] + "   ");
             }
             System.out.println();
         }
@@ -70,7 +93,7 @@ public class Main {
     //     но само със заявките за този служител.
     public static void printEmlpoyeeLeaveRequests(String name, String[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+            for (int j = 0; j < 6; j++) {
                 if (name.equals(matrix[i][0])) {
                     System.out.print(matrix[i][j] + "   ");
                 }
@@ -78,74 +101,77 @@ public class Main {
             System.out.println();
         }
     }
-/*Всички заявки да си имат уникален номер. Към таблиците от опция 2 и 3 да се добави и нова колона “Статус”.
-     Статусът може да бъде approved, rejected or pending. При избиране на 4, излиза таблица подобна на опция 2,
-     но с допълнителна колона - номер на заявката. Потребителят може да въведе номер на заявка и статус,
-     което ще промени статуса на съответната заявка.
 
-    public static String [] [] copiesMatrix(int numEmployes, String[][] matrixLeaveData) {
-        String [] [] newMatrix = new String[numEmployes][matrixLeaveData[0].length+3];
-        for (int i=0; i< matrixLeaveData.length; i++) {
-            for (int j = 0; j < matrixLeaveData[0].length; j++) {
-               newMatrix[i][j]=matrixLeaveData[i][j];
+    /*Всички заявки да си имат уникален номер. Към таблиците от опция 2 и 3 да се добави и нова колона “Статус”.
+         Статусът може да бъде approved, rejected or pending. При избиране на 4, излиза таблица подобна на опция 2,
+         но с допълнителна колона - номер на заявката. Потребителят може да въведе номер на заявка и статус,
+         което ще промени статуса на съответната заявка.
+         */
+
+    public static void printMatrixWithlIDAndStatus(int numLeaveRequests, String[][] matrixWithIDAndStatus) {
+        printAllLeaveRequestsWithIdAndStatus(matrixWithIDAndStatus);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Start change leave requests status. For exit enter 0:");
+        System.out.print("Enter leave request id:");
+        int leaveRequestId = scan.nextInt();
+        System.out.print("Change leave request status (approved or rejected):");
+        String fake = scan.nextLine();
+        String leaveRequestStatus = scan.nextLine();
+
+        do {
+            for (int i = 0; i < matrixWithIDAndStatus.length; i++) {
+                for (int j = 0; j < matrixWithIDAndStatus[0].length; j++) {
+                    if (matrixWithIDAndStatus[i][6].equals(Integer.toString(leaveRequestId))) {
+                        matrixWithIDAndStatus[i][7] = leaveRequestStatus;
+                    }
                 }
             }
-            System.out.println();
-        }
+            System.out.print("Enter leave request id:");
+            leaveRequestId = scan.nextInt();
+            fake = scan.nextLine();
+            System.out.print("Change leave request status (approved or rejected):");
+            leaveRequestStatus = scan.nextLine();
+        } while (leaveRequestId != 0);
 
+        printAllLeaveRequestsWithIdAndStatus(matrixWithIDAndStatus);
     }
-
- */
-
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Система за заявяване на отпуски\nВъведи брой служители в компанията: ");
+        System.out.print("Leave management application\nEnter number of requested leaves: ");
 
-        int numEmployes = scan.nextInt();
+        int numLeaveRequests = scan.nextInt();
         String[] employee = new String[6];
-        String[][] matrix = new String[numEmployes][6];
+        String[][] matrix = new String[numLeaveRequests][8];
         String name;
         String fake;
 
-        /*try {
-            File file = new File("C:\\newfile.txt");
-            boolean isCreated = file.createNewFile();
-            if (isCreated) {
-                System.out.println("File has been created successfully");
-            } else {
-                System.out.println("File already present at the specified location");
-            }
-        } catch (IOException e) {
-            System.out.println("Exception Occurred:");
-            e.printStackTrace();
-        }*/
-
         printMenu();
-        System.out.print("Въведи избор: ");
+        System.out.print("Enter a choice: ");
         int choice = scan.nextInt();
         int i = 0;
         while (choice != 5) {
             switch (choice) {
                 case 1:
-                    matrix = fillMatrixLeaveData(numEmployes);
+                    matrix = fillMatrixLeaveData(numLeaveRequests);
                     break;
                 case 2:
                     printAllLeaveRequests(matrix);
                     break;
                 case 3:
-                    System.out.print("Въведи име на служител: ");
+                    System.out.print("Enter an employee name: ");
                     fake = scan.nextLine();
                     name = scan.nextLine();
                     printEmlpoyeeLeaveRequests(name, matrix);
                     break;
                 case 4:
+                    printMatrixWithlIDAndStatus(numLeaveRequests, matrix);
+                    break;
 
             }
-            System.out.print("Въведи избор: ");
+            System.out.print("Enter a choice: ");
             choice = scan.nextInt();
         }
-
-
     }
 }
